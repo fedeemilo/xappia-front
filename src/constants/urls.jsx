cc71e213fdc78b372e62b15014f8748134f4a38c
Federico Milone <federico.milone@naranja.com>
2022-10-08 01:17:33 -0300

    update

--- src/constants/urls.jsx
+++ src/constants/urls.jsx
@@ -13,4 +13,4 @@ export const URLS = {
 export const URL_BACKEND = host =>
     host.includes("localhost")
         ? "http://localhost:8000"
-        : "http://ec2-54-90-45-162.compute-1.amazonaws.com:8000";
+        : "http://ec2-13-58-180-105.us-east-2.compute.amazonaws.com:8000";
